777b5b41c2b1fb824e6d982f4936a64e1e71fb1d
Lucas Alves <lucasasbeghen@hotmail.com>
2022-04-02 17:49:13 -0300

    modificando parametro da rota

--- routes/IndexRouter.js
+++ routes/IndexRouter.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var IndexController = require('../controllers/IndexController');
 
-router.get('/',IndexController.showSeguidores);
-router.get('/:login',IndexController.showSeguidores);
+router.get('/:login?',IndexController.showSeguidores);
 
 module.exports = router;
